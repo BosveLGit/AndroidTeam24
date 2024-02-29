3de531f02eeb14676771f0acb268db12ed51b459
BosveLGit <d.krysyun@yandex.com>
2024-02-29 23:15:26 +0200

    ну почти

--- Homework3/app/src/main/java/com/example/homework3/MainActivity.java
+++ Homework3/app/src/main/java/com/example/homework3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.homework3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView MainListView = findViewById(R.id.MainListView);
+        String[] counties = getResources().getStringArray(R.array.counties);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.test_list_item, counties);
+
+        // почему эта херовина не растягивается? Указал же padding
+        // почему нельзя выбрать new_item_list ??? один и тот же код!
+
+        MainListView.setAdapter(adapter);
+
     }
 }
