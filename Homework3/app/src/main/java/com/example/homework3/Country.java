15c9319a638baef4d49f20673622aa9c67a8a31c
BosveLGit <d.krysyun@yandex.com>
2024-03-03 12:33:11 +0200

    ДЗ 4

--- Homework3/app/src/main/java/com/example/homework3/Country.java
+++ Homework3/app/src/main/java/com/example/homework3/Country.java
@@ -1,13 +1,19 @@
 package com.example.homework3;
 
-public class Country {
+import java.io.Serializable;
+
+public class Country implements Serializable {
 
     private String country;
     private int iconFlag;
+    private String capital;
+    private int square;
 
-    public Country(String country, int iconFlag) {
+    public Country(String country, int iconFlag, String capital, int square) {
         this.country = country;
         this.iconFlag = iconFlag;
+        this.capital = capital;
+        this.square = square;
     }
 
     public String getCountry() {
@@ -26,11 +32,29 @@ public class Country {
         this.iconFlag = iconFlag;
     }
 
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public int getSquare() {
+        return square;
+    }
+
+    public void setSquare(int square) {
+        this.square = square;
+    }
+
     @Override
     public String toString() {
         return "Country{" +
                 "country='" + country + '\'' +
                 ", iconFlag=" + iconFlag +
+                ", capital='" + capital + '\'' +
+                ", square=" + square +
                 '}';
     }
 }
--- Homework3/app/src/main/java/com/example/homework3/MainActivity.java
+++ Homework3/app/src/main/java/com/example/homework3/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.homework3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,21 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listCountry.add(new Country("Бельгия", R.drawable.be));
-        listCountry.add(new Country("Канада", R.drawable.ca));
-        listCountry.add(new Country("Дания", R.drawable.dk));
-        listCountry.add(new Country("Эстония", R.drawable.ee));
-        listCountry.add(new Country("Франция", R.drawable.fr));
-        listCountry.add(new Country("Грузия", R.drawable.ge));
-        listCountry.add(new Country("Хорватия", R.drawable.hr));
-        listCountry.add(new Country("Венгрия", R.drawable.hu));
-        listCountry.add(new Country("Ирландия", R.drawable.ie));
-        listCountry.add(new Country("Япония", R.drawable.jp));
+        listCountry.add(new Country("Бельгия", R.drawable.be, "Брюссель", 30688));
+        listCountry.add(new Country("Канада", R.drawable.ca, "Оттава", 9985000));
+        listCountry.add(new Country("Дания", R.drawable.dk, "Копенгаген", 42952));
+        listCountry.add(new Country("Эстония", R.drawable.ee, "Таллин", 45339));
+        listCountry.add(new Country("Франция", R.drawable.fr, "Париж", 643801));
+        listCountry.add(new Country("Грузия", R.drawable.ge, "Тбилиси", 69700));
+        listCountry.add(new Country("Хорватия", R.drawable.hr, "Загреб", 56594));
+        listCountry.add(new Country("Венгрия", R.drawable.hu, "Будапешт", 93026));
+        listCountry.add(new Country("Ирландия", R.drawable.ie, "Дублин", 70273));
+        listCountry.add(new Country("Япония", R.drawable.jp, "Токио", 377973));
 
         listView = (ListView) findViewById(R.id.MainListView);
         CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(MainActivity.this, listCountry);
-        listView.setAdapter(customBaseAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, Card_country.class);
+                intent.putExtra(Country.class.getSimpleName(), listCountry.get(position));
+                startActivity(intent);
+            }
+        });
+
+        listView.setAdapter(customBaseAdapter);
 
     }
 }
